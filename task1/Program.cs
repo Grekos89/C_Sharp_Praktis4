e9ddd70753a081076cd3358416a1a90dd112e1e5
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2023-01-24 20:23:07 +0300

    метод с возвратом

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int num1 = 7;
+Console.WriteLine(GetSum(num1));
+static int GetSum(int num)
+{
+    int res = 0;
+    for(int i = 0; i <= num; i++)
+    {
+        res += i;
+    }
+    return res;
+}
