a49565c2f0122d501917c626330740a69a529953
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2023-01-24 20:33:25 +0300

    метод с возвратом2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿//колличество чисел в цифре
+Console.WriteLine(GetCountNum(new Random().Next(100, 100000)));
+static int GetCountNum(int num)
+{
+    Console.WriteLine(num);
+    int count = 0;
+    while(num > 0)
+    {
+        num /=10;
+        count++;
+    }
+    return count;
+}
