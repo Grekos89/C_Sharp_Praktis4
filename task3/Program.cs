c46b4192d82d8f017aae8ba942c7a307cca1c426
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2023-01-24 20:50:31 +0300

    создание метода с массивами

---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿//написать задачу заполняющую массив 0 и 1
+// [0, 1, 1, 0, 1] пример
+
+//PrintArray(GetRandomArray(8));
+
+int[] arr = GetRandomArray(10);
+PrintArray(arr);
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result; 
+}
+
+
+void PrintArray(int[] arr)
+{
+    
+    foreach(int item in arr)
+    {
+        System.Console.WriteLine(item);
+    } 
+}
